a6a33cfd41a400f772e6d188c9e059bbf043074c
Aleksey <63102656+leshik9@users.noreply.github.com>
2020-10-23 11:07:37 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -18,6 +18,9 @@ namespace salonv
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new intro());
 
+            
+            привет
+            
         }
     }
 }
